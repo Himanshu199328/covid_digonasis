d9a8db5afd6e259d0855255cda88a05b3b4aa8a6
Himanshu199328 <himanshu@zemosolabs.com>
2022-07-11 19:15:25 +0530

    project scaffolding

---
+++ cloud-gateway/src/test/java/com/covid/digonasis/cloudgateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covid.digonasis.cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
